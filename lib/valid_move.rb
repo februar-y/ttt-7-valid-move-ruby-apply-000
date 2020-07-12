c24aa7ca9eb378f8cd676898b2e03b7fea451135
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-12 23:36:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if (board[index] == " " or board[index] == "" or board[index] == nil) and index.between?)
+  if (board[index] == " " or board[index] == "" or board[index] == nil) and index.between?(0,8)
     true
   elsif board[index] == "X" or board[index] == "O"
     false
