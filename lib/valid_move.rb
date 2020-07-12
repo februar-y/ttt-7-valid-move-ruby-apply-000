f3bf12ff0f3b0e227f51e0b61aa3f9daf8e8d317
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-12 23:32:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,16 @@
 # code your #valid_move? method here
-
+def valid_move(board, index)
+  if position_taken?
+    false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def valid_move(board, index)
-  if (board[index] == " " or board[index] == "" or board[index] == nil) and index.between?(0,8)
-    true
-  elsif board[index] == "X" or board[index] == "O"
+def position_taken?(board, index)
+  if board[index] == " " or board[index] == "" or board[index] == nil
     false
+  elsif board[index] == "X" or board[index] == "O"
+    true
   end
 end
